42d9100714ad6c2aa009bc5b8e2fe94508b3b748
up1 <somkiat.p@gmail.com>
2021-04-21 11:58:11 +0700

    Update

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
-function App() {
-  return (
-    <div>
-      <h2>Hello React ...</h2>
-    </div>
-  );
+import { Component } from "react";
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <h2>Hello React from class ...</h2>
+      </div>
+    );
+  }
 }
 
 export default App;
